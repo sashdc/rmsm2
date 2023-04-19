bf9d7ed6540077d85e44eab76c1c9e31eeb2f4f2
Saharsh Cherian <saharshdc@gmail.com>
2023-04-19 11:42:44 -0400

    making tabs responsive

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -11,13 +11,13 @@ const About = () => {
   return (
     <div className="page-container animate__animated animate__fadeIn">
      
-      <div className=" about-section   ">
+      <div className=" about-page">
         <img src={fax} alt="fax paper" className="fax-paper " />
-          
-
-        <div className="about-body">
         <img src={about1} alt="about1" className="about-picture about1" />
         <img src={about2} alt="about1" className="about-picture about2" />  
+
+        <div className="about-body">
+       
           <h1>About</h1>
           <p className="about-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -28,7 +28,7 @@ const About = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className="about-text">
+          {/* <p className="about-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -36,7 +36,7 @@ const About = () => {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          </p> */}
           <p className="about-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
