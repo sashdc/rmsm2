47a5d488c9d2d9228ce9685a05183c8c1d8dd6a5
Saharsh Cherian <saharshdc@gmail.com>
2023-04-19 10:33:22 -0400

    added images to aboutt section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,20 +1,55 @@
 import React from "react";
 import fax from "../../assets/images/fax.png";
 import stamp from "../../assets/images/stamp.png";
+import about1 from "../../assets/images/polaroid1.png";
+import about2 from "../../assets/images/polaroid2.png";
+
+
 import "./about.css";
 
 const About = () => {
   return (
-    <div className="page-container">
-      <div className=" about-section animate__animated animate__fadeIn  ">
+    <div className="page-container animate__animated animate__fadeIn">
+     <img src={about1} alt="about1" className="about-picture about1" />
+        <img src={about2} alt="about1" className="about-picture about2" />  
+      <div className=" about-section   ">
         <img src={fax} alt="fax paper" className="fax-paper " />
-        <div className="about-text">
-        <h1>About</h1>
+          
+
+        <div className="about-body">
+          <h1>About</h1>
+          <p className="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="about-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
         <img src={stamp} alt="logo stamp" className="logo-stamp" />
       </div>
     </div>
   );
-}
+};
 
-export default About
+export default About;
